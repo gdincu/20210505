7f6a1934117d934f48b9c34f0668fb434fee182e
NRO\I.G.DINCU <ionut.dincu@netrom.ro>
2021-05-05 15:21:20 +0300

    20210505 - DateTime problem set

--- 20210505/Program.cs
+++ 20210505/Program.cs
@@ -115,6 +115,126 @@ namespace _20210505
                 rezFinal += rezBinar[i];
             Console.Write(rezFinal);
 
+            /*
+             * Sa se scrie un program care citeste o data de la tastatura si determina ziua saptamanii in care se afla acea data. E.g. Dati ziua: 3 Dati luna: 5 Dati anul: 2021 Aceasta data se afla intr-o zi de luni.
+            */
+            
+            Console.Write("\n11)Zi/Luna/An: ");
+            DateTime tt = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Aceasta data se afla intr - o zi de ");
+            switch (tt.DayOfWeek.ToString())
+            {
+                case "Monday":
+                    Console.Write("Luni");
+                    break;
+                case "Tuesday":
+                    Console.Write("Marti");
+                    break;
+                case "Wednesday":
+                    Console.Write("Miercuri");
+                    break;
+                case "Thursday":
+                    Console.Write("Joi");
+                    break;
+                case "Friday":
+                    Console.Write("Vineri");
+                    break;
+                case "Saturday":
+                    Console.Write("Sambata");
+                    break;
+                case "Sunday":
+                    Console.Write("Duminica");
+                    break;
+                default:
+                    Console.Write(" ");
+                    break;
+            }
+
+
+            /*
+             * Sa se citeasca o data, sa se determine care este ultima zi din luna acele date
+             */
+            Console.Write("\n12) Ultima zi din luna datei alese este: ");
+            switch(tt.Month)
+            {
+                case 1:
+                    Console.Write("31/"+tt.Month+"/"+tt.Year);
+                    break;
+                case 2:
+                    if (DateTime.IsLeapYear(tt.Year))
+                    Console.Write("29/" + tt.Month + "/" + tt.Year);
+                    else
+                    Console.Write("28/" + tt.Month + "/" + tt.Year);
+                    break;
+                case 3:
+                    Console.Write("31/" + tt.Month + "/" + tt.Year);
+                    break;
+                case 4:
+                    Console.Write("30/" + tt.Month + "/" + tt.Year);
+                    break;
+                case 5:
+                    Console.Write("31/" + tt.Month + "/" + tt.Year);
+                    break;
+                case 6:
+                    Console.Write("30/" + tt.Month + "/" + tt.Year);
+                    break;
+                case 7:
+                    Console.Write("31/" + tt.Month + "/" + tt.Year);
+                    break;
+                case 8:
+                    Console.Write("31/" + tt.Month + "/" + tt.Year);
+                    break;
+                case 9:
+                    Console.Write("30/" + tt.Month + "/" + tt.Year);
+                    break;
+                case 10:
+                    Console.Write("31/" + tt.Month + "/" + tt.Year);
+                    break;
+                case 11:
+                    Console.Write("30/" + tt.Month + "/" + tt.Year);
+                    break;
+                case 12:
+                    Console.Write("31/" + tt.Month + "/" + tt.Year);
+                    break;
+                default:
+                    Console.WriteLine(" ");
+                    break;
+            }
+
+            /*
+             * Se citesc doua date. Sa se determine care este cea mai recentă.
+             */
+            Console.Write("\n13)Zi/Luna/An: ");
+            DateTime tt1 = DateTime.Parse(Console.ReadLine());
+            Console.Write("\nZi/Luna/An: ");
+            tt = DateTime.Parse(Console.ReadLine());
+            if (tt1.CompareTo(tt) > 0)
+                Console.WriteLine(tt1 + " este mai recenta decat " + tt);
+            else
+                Console.WriteLine(tt + " este mai recenta decat " + tt1);
+
+            /*
+             * Sa se citeasca o data si sa se converteasca: 
+             * in short time string representation
+             * ln long time string representation
+             * in formatul unei alte culturi (E.g. franceza, engleza)
+             * in universal time format (UTC)
+             */
+            Console.Write("\n14)Zi/Luna/An: ");
+            tt = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("\nShortTimeString: " + tt.ToShortTimeString());
+            Console.Write("\nLongTimeString: " + tt.ToLongTimeString());
+            Console.Write("\nLocalFormat: " + tt.ToString(System.Globalization.CultureInfo.CreateSpecificCulture("fr-FR")));
+            Console.Write("\nUTC: " + tt.ToUniversalTime());
+
+
+
+
+
+
+
 
         }
     }
